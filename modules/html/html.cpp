743e051294cc7ba1e94457f0f9a6cdfb80a2cc36
Cedric Hamelin <cedric.m.hamelin@gmail.com>
2017-03-21 09:19:46 +0100

    Makefile minor modifications to make deps, html.cpp > attempt to upgrade from CEF

--- modules/html/html.cpp
+++ modules/html/html.cpp
@@ -197,7 +197,22 @@ public:
 			{
 				CefRefPtr<CefV8Value> ret;
 				CefRefPtr<CefV8Exception> exception;
-				context->Eval("tickAnimations()", ret, exception);
+				//context->Eval("tickAnimations()", ret, exception);
+                context->Eval("tickAnimations()",nullptr,0, ret, exception);
+                /*
+                 virtual bool Eval(const CefString& code,
+                 CefRefPtr<CefV8Value>& retval,
+                 CefRefPtr<CefV8Exception>& exception) =0;
+                 
+                 
+                 virtual bool Eval(const CefString& code,
+                 const CefString& script_url,
+                 int start_line,
+                 CefRefPtr<CefV8Value>& retval,
+                 CefRefPtr<CefV8Exception>& exception) =0;
+                 */
+                
+                
 			}
 
 			return true;
